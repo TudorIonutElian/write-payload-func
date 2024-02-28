2ce15116b057ded33f57084d6816aba5fa26d16c
Ionut Elian TUDOR <ionuteliantudor@gmail.com>
2024-02-29 00:07:03 +0200

    Add camelCase logic for file

--- generateFileContent.js
+++ generateFileContent.js
@@ -3,15 +3,15 @@ const generateFileContent = (event) => {
     for(let i = 0; i < 20; i++) {
       stringToWrite += `requestType: ${event.requestType}, `;
       stringToWrite += `requestUrl: ${event.requestUrl}, `;
-      stringToWrite += `request_ip: ${event.requestValues.request_ip}, `;
-      stringToWrite += `request_vpc: ${event.requestValues.request_vpc}, `;
-      stringToWrite += `request_region: ${event.requestValues.request_region}, `;
-      stringToWrite += `request_availability_zone: ${event.requestValues.request_availability_zone},`;
-      stringToWrite += `request_iam_role: ${event.requestValues.request_iam_role}, `;
-      stringToWrite += `request_api_key: ${event.requestValues.request_api_key}, `;
-      stringToWrite += `request_username: ${event.requestValues.request_username}, `;
-      stringToWrite += `request_authorization_policy: ${event.requestValues.request_authorization_policy}, `;
-      stringToWrite += `request_scanned: ${event.requestValues.request_scanned}\n`;
+      stringToWrite += `requestIp: ${event.requestValues.request_ip}, `;
+      stringToWrite += `requestVpc: ${event.requestValues.request_vpc}, `;
+      stringToWrite += `requestRegion: ${event.requestValues.request_region}, `;
+      stringToWrite += `requestAvailabilityZone: ${event.requestValues.request_availability_zone},`;
+      stringToWrite += `requestIamRole: ${event.requestValues.request_iam_role}, `;
+      stringToWrite += `requestApiKey: ${event.requestValues.request_api_key}, `;
+      stringToWrite += `requestUsername: ${event.requestValues.request_username}, `;
+      stringToWrite += `requestAuthorizationPolicy: ${event.requestValues.request_authorization_policy}, `;
+      stringToWrite += `requestScanned: ${event.requestValues.request_scanned}\n`;
     }
 
     return stringToWrite;
