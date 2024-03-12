6c86337619c450778c31d4daf49463f3b7ab5a5d
Ionut Elian TUDOR <ionuteliantudor@gmail.com>
2024-03-12 22:23:04 +0200

    Remove ticks

--- index.js
+++ index.js
@@ -26,29 +26,27 @@ exports.handler = async (event) => {
   const generatedFileContent = generateFileContent(event);
   console.log('File content generated successfully!');
   response.body.generatedFileContent = true;
-  response.body.messages.push(`'File content generated successfully at ${new Date().toISOString()}'`);
+  response.body.messages.push(`File content generated successfully at ${new Date().toISOString()}`);
 
   saveLocalFile(generatedFileContent);
   response.body.fileLocallySaved = true;
-  response.body.messages.push(`'File saved locally at ${new Date().toISOString()}'`);
+  response.body.messages.push(`File saved locally at ${new Date().toISOString()}`);
   console.log('File saved locally successfully!');
 
 
   await uploadToFirstS3(generatedFileContent);
   response.body.fileUploadedToS3 = true;
   response.body.logsSaved = true;
-  response.body.messages.push(`'File uploaded to S3 Bucket at ${new Date().toISOString()}'`);
-  response.body.messages.push(`'Logs saved at ${new Date().toISOString()}'`);
+  response.body.messages.push(`File uploaded to S3 Bucket at ${new Date().toISOString()}`);
+  response.body.messages.push(`Logs saved at ${new Date().toISOString()}`);
   console.log('File uploaded to S3 Bucket successfully!');
 
   const tmpDirectory = '/tmp';
   const temporaryFilePath = `${tmpDirectory}/request.txt`;
   fs.unlinkSync(temporaryFilePath);
   response.body.fileLocallyDeleted = true;
-  response.body.messages.push(`'File deleted from ${temporaryFilePath} at ${new Date().toISOString()}'`);
+  response.body.messages.push(`File deleted from ${temporaryFilePath} at ${new Date().toISOString()}`);
   console.log(`File deleted from ${temporaryFilePath}`);
-
-  response.body = JSON.stringify(response.body);
   
   return response;
 };
