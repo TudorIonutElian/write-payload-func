80d8af79bb1c9b5f96a2d7fce1f5f3caa891568f
Ionut Elian TUDOR <ionuteliantudor@gmail.com>
2024-03-12 22:17:24 +0200

    Update the response

--- index.js
+++ index.js
@@ -7,26 +7,40 @@ exports.handler = async (event) => {
     // Your Lambda function logic here
   
     const response = {
-      statusCode: 200,
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify({
-      message: JSON.stringify(event),
-      // Add more data as needed
-    })
-  };
+        statusCode: 200,
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify(
+            {
+              status: undefined,
+              messages: [],
+              generatedFileContent: false,
+              fileLocallySaved: false,
+              fileLocallyDeleted: false,
+              fileUploadedToS3: false,
+              logsSaved: false
+            }
+          )
+    };
 
   const generatedFileContent = generateFileContent(event);
+  console.log('File content generated successfully!');
+  response.generatedFileContent = true;
+
   saveLocalFile(generatedFileContent);
+  response.fileLocallySaved = true;
+  console.log('File saved locally successfully!');
+
 
   await uploadToFirstS3(generatedFileContent);
+  response.fileUploadedToS3 = true;
   console.log('File uploaded to S3 Bucket successfully!');
 
   const tmpDirectory = '/tmp';
   const temporaryFilePath = `${tmpDirectory}/request.txt`;
   fs.unlinkSync(temporaryFilePath);
-
+  response.fileLocallyDeleted = true;
   console.log(`File deleted from ${temporaryFilePath}`);
   
   return response;
