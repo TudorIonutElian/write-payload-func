1ab0f3cbc3d1694d1bba7e7a90cff678dbf7b520
Ionut Elian TUDOR <ionuteliantudor@gmail.com>
2024-03-12 22:21:09 +0200

    Change the response logic

--- index.js
+++ index.js
@@ -11,37 +11,44 @@ exports.handler = async (event) => {
         headers: {
           "Content-Type": "application/json"
         },
-        body: JSON.stringify(
-            {
-              status: undefined,
-              messages: [],
-              generatedFileContent: false,
-              fileLocallySaved: false,
-              fileLocallyDeleted: false,
-              fileUploadedToS3: false,
-              logsSaved: false
-            }
-          )
+        body: {
+            status: undefined,
+            messages: [],
+            generatedFileContent: false,
+            fileLocallySaved: false,
+            fileLocallyDeleted: false,
+            fileUploadedToS3: false,
+            logsSaved: false
+          }
+
     };
 
   const generatedFileContent = generateFileContent(event);
   console.log('File content generated successfully!');
-  response.generatedFileContent = true;
+  response.body.generatedFileContent = true;
+  response.body.messages.push(`'File content generated successfully at ${new Date().toISOString()}'`);
 
   saveLocalFile(generatedFileContent);
-  response.fileLocallySaved = true;
+  response.body.fileLocallySaved = true;
+  response.body.messages.push(`'File saved locally at ${new Date().toISOString()}'`);
   console.log('File saved locally successfully!');
 
 
   await uploadToFirstS3(generatedFileContent);
-  response.fileUploadedToS3 = true;
+  response.body.fileUploadedToS3 = true;
+  response.body.logsSaved = true;
+  response.body.messages.push(`'File uploaded to S3 Bucket at ${new Date().toISOString()}'`);
+  response.body.messages.push(`'Logs saved at ${new Date().toISOString()}'`);
   console.log('File uploaded to S3 Bucket successfully!');
 
   const tmpDirectory = '/tmp';
   const temporaryFilePath = `${tmpDirectory}/request.txt`;
   fs.unlinkSync(temporaryFilePath);
-  response.fileLocallyDeleted = true;
+  response.body.fileLocallyDeleted = true;
+  response.body.messages.push(`'File deleted from ${temporaryFilePath} at ${new Date().toISOString()}'`);
   console.log(`File deleted from ${temporaryFilePath}`);
+
+  response.body = JSON.stringify(response.body);
   
   return response;
 };
